1fc0e4a3f83436ecdbf708decaa0ea8e5bacdc4b
Mohammed Raja <mohammed.raja@lookout.com>
2020-01-11 02:27:48 +0530

    Creating areaChart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { AreaChartComponent } from './area-chart/area-chart.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AreaChartComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
