7d7d3156692a2b8b8a14e7593c5316a2b9de263c
Mohammed Raja <mohammed2raja@gmail.com>
2020-01-13 03:15:18 +0530

    creating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { d3 } from "d3";
 
 @Component({
   selector: "app-root",
@@ -7,4 +8,10 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   title = "CodeSandbox";
+  svg = d3
+    .select("div#container")
+    .append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 300 300")
+    .classed("svg-content", true);
 }
